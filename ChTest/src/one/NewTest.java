374d21a2a6232791cd24357e379e703984e45ffb
simplylearn <g4.nirupam@gmail.com>
2017-04-02 10:50:35 +0530

    testing

---
+++ ChTest/src/one/NewTest.java
@@ -0,0 +1,63 @@
+package one;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class NewTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void beforetest1() throws InterruptedException
+	{
+		
+		 System.out.println("in beforetest");
+		 driver = new FirefoxDriver();
+
+		 driver.get("http://google.co.in");
+		  Thread.sleep(10000);
+	}
+/*	
+  @Test
+  public void test1() throws InterruptedException {
+	  
+	  System.out.println("testing...");
+	 // driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Cheese");   
+	 // Thread.sleep(10000);
+	  
+	  driver.findElement(By.id("lst-ib1")).sendKeys("India");
+
+	  Thread.sleep(7000);
+	  driver.findElement(By.name("btnG")).click();
+	  Thread.sleep(7000);
+	  
+  }
+  */
+  @Test(invocationCount = 1)
+  public void tes() throws InterruptedException
+  {
+
+	  driver.findElement(By.id("lst-ib")).sendKeys("India");
+	  Thread.sleep(7000);
+	  	  
+	  driver.findElement(By.name("btnG")).click();
+	  Thread.sleep(7000);
+	  
+	  
+	//  driver.
+	  
+  }
+ 
+ @AfterTest
+ public void after()
+ {
+	 
+	 driver.quit();
+ }
+ 
+}
